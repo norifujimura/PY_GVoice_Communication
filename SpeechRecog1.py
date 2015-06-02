93ba7bf5c73ef71378373f5431e2d637407c76d1
nori <nori_fujimura@me.com>
2015-06-02 15:59:40 +0900

    dummy change
    dummy change

--- SpeechRecog1.py
+++ SpeechRecog1.py
@@ -1,4 +1,4 @@
-#dummy change
+#dummy change 2
                                                # NOTE: this requires PyAudio because it uses the Microphone class
 
 #only works from command line. not from IDLE
